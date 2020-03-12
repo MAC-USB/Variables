6662b274daebe0f7ac9ba35cd792726e568ddb2d
Carlos Rivero <carlosrivero96@gmail.com>
2020-03-12 00:48:33 -0400

    Added transitions in RandomEncounters

--- Assets/BattleTransitions/SimpleBlit.cs
+++ Assets/BattleTransitions/SimpleBlit.cs
@@ -6,6 +6,10 @@ public class SimpleBlit : MonoBehaviour
 {
     public Material TransitionMaterial;
 
+    void Start(){
+        Debug.Log(TransitionMaterial.GetFloat("_Cutoff"));
+    }
+
     void OnRenderImage(RenderTexture src, RenderTexture dst)
     {
         if (TransitionMaterial != null)
