6888374919aa4407e8779d481314a1d0749f0b80
Juan Oropeza <33599807+jaoc1811@users.noreply.github.com>
2020-03-12 06:05:27 -0400

    Cosas

--- Assets/JO/Scripts/RandomEncounter.cs
+++ Assets/JO/Scripts/RandomEncounter.cs
@@ -97,7 +97,7 @@ public class RandomEncounter : MonoBehaviour
             bool moved = prevPos != gameObject.transform.position;
             if (moved)
             {
-                int chance = Random.Range(0,1000);
+                int chance = Random.Range(0,1000000);
                 prevPos = gameObject.transform.position;
                 //Debug.Log(chance);
 
