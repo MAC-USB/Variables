03c3a6ce3cfcda4740c5f7ae4e3f5d0d42396c97
JRBarreraM <mrjrbarreram@gmail.com>
2020-03-11 21:13:20 -0400

    melchor LeaderBoard

---
+++ Assets/Melchor/EnableDisable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisable : MonoBehaviour
+{
+    private bool disabled = false;
+    public GameObject objectToDisable;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)){
+            objectToDisable.active = !(objectToDisable.active);
+        }
+    }
+}
---
+++ Assets/Melchor/Variables.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{   
+    // // Juancito
+    // public Vector3 position = new Vector3();
+    // public Dictionary<string, int> portales = new Dictionary<string, int>();
+
+    // // Carlitos
+    // public int currentChallenge;
+    // public bool challengeCompleted;
+    // public string sceneName;
+    // public List<int> challengesCompletedElyiano;
+    // public List<int> challengesCompletedMagicant;
+    // public List<int> challengesCompletedLaPuta;
+    // public List<int> challengesCompletedKonohagakure;
+    // public List<int> challengesCompletedNeovice;
+    // public List<int> challengesCompletedKernel;
+
+    //Barrera
+    public int score = 1000;
+    public string username = "Juan El egro";
+    
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     portales.Add("Elyiano",0);
+    //     portales.Add("Magicant",0);
+    //     portales.Add("LaPuta",0);
+    //     portales.Add("Konohagakure",0);
+    //     portales.Add("Neovice",0);
+    //     portales.Add("Kernel",0);
+        
+    //     DontDestroyOnLoad(gameObject);
+
+    //     if (FindObjectsOfType(GetType()).Length > 1)
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    // }
+
+    // Update is called once per frame
+    
+    void Start()
+    {
+        InvokeRepeating("Sum1", 1.0f, 1.0f);
+    }
+    void Sum1()
+    {
+        score += 1;
+        
+    }
+}
