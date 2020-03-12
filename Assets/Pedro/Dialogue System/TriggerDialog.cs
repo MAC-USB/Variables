ff00c7088e012e96da681229670c71c0171d10f9
xdgorgola <15-11264@usb.ve>
2020-03-11 21:44:18 -0400

    Dialog by trigger

---
+++ Assets/Pedro/Dialogue System/TriggerDialog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider2D))]
+public class TriggerDialog : MonoBehaviour
+{
+    public ConversationSO conversation = null;
+    private Collider2D coll2d = null;
+    private bool wasActivated = false;
+
+    private void Awake()
+    {
+        coll2d = GetComponent<Collider2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!coll2d.isTrigger) Debug.LogWarning("El trigger de dialogo no es trigger :(", gameObject);
+        if (conversation == null) Debug.LogWarning("El trigger de dialogo no tiene dialogo :(", gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Solo deberia colisionar el jugador
+        if (!wasActivated)
+        {
+            Debug.Log("Si lees esto eres marico oyo");
+            DialogSystem.Manager.StartConversation(conversation);
+            wasActivated = true;
+        }
+    }
+}
