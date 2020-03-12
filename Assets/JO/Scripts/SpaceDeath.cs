270efe5d30889e1b829f888ca06a9745d9e4c3f3
Juan Oropeza <33599807+jaoc1811@users.noreply.github.com>
2020-03-12 06:18:10 -0400

    Triggers

---
+++ Assets/JO/Scripts/SpaceDeath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceDeath : MonoBehaviour
+{
+
+    bool fix = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fix && Input.GetKeyDown(KeyCode.F)){
+            Variables.managers.current_hp = 0;
+            StartCoroutine(SimpleBlit.managers.FadeOut(TransType.Portal, "Elyiano"));
+            //SceneManager.LoadScene(to);
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Player") fix = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        if (other.tag == "Player") fix = false;
+    }
+}
