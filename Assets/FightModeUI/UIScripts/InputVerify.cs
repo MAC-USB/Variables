f92900492b17a5f1eb5b9acbd09df39fc31ab735
Carlos Sivira <carlos.csivira@gmail.com>
2020-03-11 22:26:27 -0400

    Attack Mode Updated

---
+++ Assets/FightModeUI/UIScripts/InputVerify.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputVerify : MonoBehaviour
+{
+    public FightUIManager ui_manager;
+
+    private void Start(){
+        ui_manager = GameObject.Find("MainPanel").GetComponent<FightUIManager>();
+    }
+
+    private void Update(){
+        if(ui_manager.current_ui_mode == UIMode.attackMode && Input.GetKeyDown(KeyCode.Return)){
+            if(checkAnswer(ui_manager.monster.solution)){
+                //Respuesta
+                //Ganar y salir de combate. Secuencia de escape
+                ui_manager.manager.challengeCompleted = true;
+            } else {
+                //Perder vida
+                //Barra de vida
+                Debug.Log("Fail");
+                ui_manager.updateUI(UIMode.initialMode);
+            }
+        }
+    }
+
+    private bool checkAnswer(string answer){
+        return GetComponent<InputField>().text == answer;
+    }
+}
